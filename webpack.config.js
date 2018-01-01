0b9774eaaaa8574862d9abb90fa2537b45d286c5
Aittaleb <hamid.man14@gmail.com>
2018-01-01 12:03:59 +0000

    update for foundation support

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,43 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
-    output: {
-      path: __dirname,
-      filename: './public/bundle.js'
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      applicationStyles: 'app/styles/app.scss'
     },
-    resolve: {
-      root : __dirname,
-       alias : {
-         
-       },
-      extensions: ['', '.js', '.jsx']
-    },
-    module: {
-      loaders: [
-        {
-          loader: 'babel-loader',
-          query: {
-            presets: ['react', 'es2015']
-          },
-          test: /\.jsx?$/,
-          exclude: /(node_modules|bower_components)/
-        }
-      ]
-    }
-  };
-  
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
